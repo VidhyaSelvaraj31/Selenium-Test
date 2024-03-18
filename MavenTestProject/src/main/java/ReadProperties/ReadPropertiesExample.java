02335eda0de2b74b2978f652e6fdc41ac430f809
Administrator <Administrator@172.31.10.20>
2024-03-18 07:55:10 +0000

    File commit

---
+++ MavenTestProject/src/main/java/ReadProperties/ReadPropertiesExample.java
@@ -0,0 +1,24 @@
+package ReadProperties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadPropertiesExample {
+
+	public Properties readpropertiesFile() throws IOException {
+		File src= new File("./configuration.properties");
+
+		FileInputStream fis = new FileInputStream(src);
+		//System.out.println(fis);
+		Properties obj= new Properties();
+
+		obj.load(fis);
+
+		
+		return obj;
+
+	}
+}
